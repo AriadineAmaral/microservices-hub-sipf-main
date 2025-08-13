2ac1447cdd0c2a1ff5bb4ba48962957229c1a04a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 09:45:07 -0300

    inclui kakfa

---
+++ ms-pedido/src/main/java/com/github/cidarosa/ms_pedido/kafka/PedidoProducer.java
@@ -0,0 +1,18 @@
+package com.github.cidarosa.ms_pedido.kafka;
+
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoProducer {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void enviarMensagem(String mensagem){
+        kafkaTemplate.send("topico-pedidos", mensagem);
+    }
+
+}
